daef207ac6269a89deda6439eaf0dff0e865284e
Fortan Pireva <fortanpireva99@gmail.com>
2020-06-11 00:16:03 +0200

    'another'

--- src/main/java/com/example/mailServer/MailController.java
+++ src/main/java/com/example/mailServer/MailController.java
@@ -1,5 +1,6 @@
 package com.example.mailServer;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ public class MailController {
 
 
     @PostMapping
-    public boolean sendEmail(@RequestBody Order order){
+    public boolean sendEmail(@JsonProperty("body") Order order){
         System.out.println(order);
         boolean rezultati=MailService.sendEmail(order);
         return rezultati;
